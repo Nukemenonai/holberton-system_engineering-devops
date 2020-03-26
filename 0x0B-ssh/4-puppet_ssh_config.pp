7e13dc78b92ac481d518fa68da17b8c7eb8198cc
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-26 10:27:57 -0500

    fix: ????

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,11 +2,11 @@
 file_line{ 'refuse':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+  line   => '    PasswordAuthentication no'
 }
 
 file_line{ 'priv_key':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentifyFile ~/.ssh/holberton',
+  line   => '    IdentifyFile ~/.ssh/holberton'
 }
