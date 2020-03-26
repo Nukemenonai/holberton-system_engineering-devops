169fc896912d24168fc98a0c488f9010ba9c8c79
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-26 10:39:42 -0500

    fix: changed order of inputs

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,18 @@
 # Puppet client configuration file
-file_line { 'refuse':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no'
-}
-
 file_line { 'priv_key':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentifyFile ~/.ssh/holberton'
 }
+
+file_line { 'refuse_pwd':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no'
+}
+
+#exec { 'echo':
+#  path => '/usr/bin:/bin',
+#  command => echo ''
+#
+#}
