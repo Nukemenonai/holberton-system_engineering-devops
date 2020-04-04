2e865c79c69757a036098685bd6b60b07be9d9d8
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-04 17:24:15 +0000

    fix: changed default to nginx.conf file

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -14,7 +14,7 @@ exec {'install_nginx':
 
 exec {'sed':
   require => Exec['install_nginx'],
-  command => 'sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By $hostname;/" /etc/nginx/sites-enabled/default',
+  command => 'sudo sed -i "70i \ \tadd_header X-Served-By $hostname;/" /etc/nginx/nginx.conf',
   path    => ['/usr/bin/','/bin'],
   returns => [1, 0]
 }
