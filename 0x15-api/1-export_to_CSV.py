18c3e5dc4d9e31b5e0d01778af685d4667283331
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-04 15:46:39 +0000

    [update] extended script 0 to export data to CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+this script uses the JSONplaceholder REST API
+given an emplyee ID, it will return his todo list data
+"""
+
+from requests import get
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+
+    base_url = "https://jsonplaceholder.typicode.com/"
+    ID = argv[1]
+
+    EMPLOYEE_NAME = get(base_url + "users",
+                        params={"id": ID}).json()[0].get('name')
+
+    TOTAL_NUMBER_OF_TASKS = get(base_url + "todos",
+                                params={"userId": ID}).json()
+
+    NUMBER_OF_DONE_TASKS = get(base_url + "todos",
+                               params={"userId": ID,
+                                       "completed": "true"}).json()
+
+    with open('{}.csv'.format(ID), 'w') as csvfile:
+        for item in TOTAL_NUMBER_OF_TASKS:
+            l = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            l.writerow([ID, EMPLOYEE_NAME, item.get('completed'),
+                        item.get('title')])
