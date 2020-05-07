af094e3acc108773a09c544910cd2ee1cce2dfdc
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-07 22:43:27 +0000

    fix: pep8

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,28 +3,30 @@
 
 import requests
 
+
 def top_ten(subreddit):
     """
     function that retrieves 10 latest post in hot listing
     """
     base = 'https://api.reddit.com/r/'
     user_agent = {'User-agent': 'david/1.0.0'}
-    r = requests.get(base + subreddit + '/hot', headers=user_agent,
-                     allow_redirects=False, params={"limit":10})
-    if (r.status_code != 200):
+    r = requests.get(base + subreddit + '/hot?limit=10', headers=user_agent,
+                     allow_redirects=False)
+    if r.status_code != 200:
         print(None)
         return None
-    else:
-        try:
-            data = r.json()
-        except:
-            print("Not a JSON")
-            return 0
-        try:
-            body = data.get('data')
-            children = body.get('children')
-            for item in children:
-                post = item.get('data')
-                print(post.get('title'))
-        except:
-            return None
+
+    try:
+        data = r.json()
+    except:
+        print("Not a valid JSON")
+        return 0
+
+    try:
+        body = data.get('data')
+        children = body.get('children')
+        for item in children:
+            post = item.get('data')
+            print(post.get('title'))
+    except:
+        return None
