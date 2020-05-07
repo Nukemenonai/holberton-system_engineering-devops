35e664b91f9660cded7d3c55f051f356b20a7697
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-07 20:16:57 +0000

    [fix]: included redirection statuses

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,11 +9,12 @@ def top_ten(subreddit):
     """
     returns the numberof subscribers of a subreddit
     """
+    status_codes = [301, 302, 404]
     base = 'https://api.reddit.com/r/'
     user_agent = {'User-agent': 'Mozilla/5.0'}
     r = requests.get(base + subreddit + '/hot', headers=user_agent,
                      allow_redirects=False, params={"limit":10})
-    if (r.status_code == 404):
+    if (r.status_code in status_codes):
         print(None)
     else:
         data = r.json()
