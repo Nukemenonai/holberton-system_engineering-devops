43a74cb1a4d8d06a514d75a65255e7cdb2d5d5b3
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-07 22:36:32 +0000

    [fix]: covered additional case of error in transformation of data to json

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,14 +7,24 @@ def top_ten(subreddit):
     """
     function that retrieves 10 latest post in hot listing
     """
-    status_codes = [301, 302, 404]
     base = 'https://api.reddit.com/r/'
-    user_agent = {'User-agent': 'david/1.0'}
+    user_agent = {'User-agent': 'david/1.0.0'}
     r = requests.get(base + subreddit + '/hot', headers=user_agent,
                      allow_redirects=False, params={"limit":10})
     if (r.status_code != 200):
         print(None)
+        return None
     else:
-        data = r.json()
-        for item in data['data']['children']:
-            print(item['data']['title'])
+        try:
+            data = r.json()
+        except:
+            print("Not a JSON")
+            return 0
+        try:
+            body = data.get('data')
+            children = body.get('children')
+            for item in children:
+                post = item.get('data')
+                print(post.get('title'))
+        except:
+            return None
