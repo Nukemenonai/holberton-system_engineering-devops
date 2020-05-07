a91eef231e699df6c15969acc03a1d2ac6a20be0
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-07 17:20:28 +0000

    retrieves the number of subs from a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" this module returns the number of subscribers """
+
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """
+    returns the numberof subscribers of a subreddit
+    """
+
+    base = 'https://reddit.com/r/'
+    user_agent = {'User-agent': 'Mozilla/5.0'}
+    r = requests.get(base + subreddit + '/about.json', headers=user_agent)
+    data = r.json()
+    return (data["data"]["subscribers"])
