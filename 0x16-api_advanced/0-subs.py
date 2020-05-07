d096bdc8457e0e0a4533e0832666454a356c107e
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-07 17:45:35 +0000

    fix: allow redirects set to false

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,8 @@ def number_of_subscribers(subreddit):
     """
     base = 'https://reddit.com/r/'
     user_agent = {'User-agent': 'Mozilla/5.0'}
-    r = requests.get(base + subreddit, headers=user_agent)
+    r = requests.get(base + subreddit, headers=user_agent,
+                     allow_redirects=False)
     if (r.status_code == 404):
         return 0
     else:
