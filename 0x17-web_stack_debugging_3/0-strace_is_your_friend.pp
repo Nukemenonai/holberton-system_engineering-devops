0095ccd4bd9d82ea07535b619202e4af6e196a78
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-19 23:02:09 +0000

    fix: added comment line

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# this puppet script fixes a typo in wp-settings file inorder to fix a WP server
 exec { 'debug':
   command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php; sudo service apache2 restart',
   path    => ['/usr/bin', '/bin'],
