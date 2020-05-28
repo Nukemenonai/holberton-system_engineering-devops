655a42f4a66f5a1fb35329f130598b549e5f7cca
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-28 22:44:30 +0000

    restart

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # this puppet script fixes a typo in wp-settings file inorder to fix a WP server
 exec { 'debug':
-  command => 'sed -i "s/worker_processes 4/worker_processes 10/" /etc/nginx/nginx.conf',
+  command => 'sed -i "s/worker_processes 4/worker_processes 10/" /etc/nginx/nginx.conf ; sudo service nginx restart',
   path    => ['/usr/bin', '/bin'],
 }
