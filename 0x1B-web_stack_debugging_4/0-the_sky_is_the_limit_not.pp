63afb3e17e66ec5569979b6060afe3e969ce7ec6
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-28 22:40:48 +0000

    [attempt]: no tests

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# this puppet script fixes a typo in wp-settings file inorder to fix a WP server
+exec { 'debug':
+  command => 'sed -i "s/worker_processes 4/worker_processes 10/" /etc/nginx/nginx.conf',
+  path    => ['/usr/bin', '/bin'],
+}
